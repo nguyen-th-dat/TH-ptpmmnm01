5520422f110203d10da6179e021c0b6c58f724ec
Nguyen Thanh Dat <it.ntdat@gmail.com>
2026-09-16 16:59:39 +0700

    first commit

---
+++ thuchanh/bai1.php
@@ -0,0 +1,15 @@
+<!-- Viết 1 trang web nhận một giá trị ngẫu nhiên là số tự nhiên N
+có giá trị từ 1 → 100. Hãy xuất ra trình duyệt những số chẵn
+nằm trong khoảng 1 → N đó. -->
+<!-- <?php
+    $var = rand(1,100);
+    echo $var;
+    echo "</br>";
+    echo "So chan tu 1 -> 100: ";
+    for ($i = 1; $i < 100; $i++) {
+        if ($i %2== 0) {
+            echo $i;
+            echo " ";
+        }
+    }
+?> -->
