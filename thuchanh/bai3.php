18d6372721fd261826627316f09fc56d7c4a4fe6
Nguyen Thanh Dat <it.ntdat@gmail.com>
2026-09-16 17:06:09 +0700

    first commit

---
+++ thuchanh/bai3.php
@@ -0,0 +1,7 @@
+<!-- Viết 1 trang web nhận một giá trị ngẫu nhiên là số tự nhiên N
+có giá trị trong [−100; 100]. Sau đó kiểm tra N có là số dương
+không? Nếu thỏa thì:
+- In ra các ước số của N.
+- Viết hàm kiểm tra xem N có phải là số nguyên tố không?
+- Tính tổng các số nguyên tố < N.
+- Kiểm tra N có là số chính phương? -->
